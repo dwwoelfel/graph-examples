72f1e84c3cf59fd6393f0992b60ee3a666a7c949
Daniel Woelfel <dwwoelfel@gmail.com>
2022-02-14 23:19:30 -0800

    saveQuery working

--- netlify/functions/saveQuery/saveQuery.ts
+++ netlify/functions/saveQuery/saveQuery.ts
@@ -1,7 +1,25 @@
-import { Handler } from "@netlify/functions";
-import { buildClientSchema, GraphQLSchema, parse } from "graphql";
+import { Handler, getSecrets } from "@netlify/functions";
+import {
+  buildClientSchema,
+  GraphQLSchema,
+  parse,
+  visitWithTypeInfo,
+  visit,
+  validate,
+  TypeInfo,
+  GraphQLOutputType,
+  isLeafType,
+  GraphQLObjectType,
+  GraphQLUnionType,
+  GraphQLInterfaceType,
+  Kind,
+} from "graphql";
+import { nanoid } from "nanoid";
 import fetch from "node-fetch";
+import { Octokit } from "octokit";
 import { fetchServiceInfo } from "./../netlifyGraph";
+import prettier from "prettier/standalone";
+import prettierGraphql from "prettier/parser-graphql";
 
 let schemaMemo: null | [number, GraphQLSchema] = null;
 
@@ -49,6 +67,7 @@ function validateBody(event) {
   try {
     const json = JSON.parse(event.body);
     const query = json.query;
+    const operationName = json.operationName;
     if (!query) {
       return new Error(
         "Invalid body. Expected key `query` with a GraphQL query."
@@ -56,9 +75,35 @@ function validateBody(event) {
     }
     try {
       const parsedQuery = parse(query);
+      let validatedOperationName;
+
+      for (const definition of parsedQuery.definitions) {
+        if (definition.kind === Kind.OPERATION_DEFINITION) {
+          if (!definition.name?.value) {
+            return new Error(
+              "Can not save query. The query must have an operation name to save it."
+            );
+          }
+          if (operationName && definition.name.value !== operationName) {
+            return new Error(
+              `Can not save query. Expected ${operationName} to be the only operation in the GraphQL query, found ${definition.name.value}.`
+            );
+          } else {
+            validatedOperationName = definition.name.value;
+          }
+        }
+      }
+      if (!validatedOperationName) {
+        return new Error(
+          `Can not save query. Did not find ${
+            operationName ? `${operationName} ` : ""
+          }operation.`
+        );
+      }
       return {
         parsedQuery,
         query,
+        operationName: validatedOperationName,
       };
     } catch (e) {
       return new Error(e);
@@ -66,6 +111,58 @@ function validateBody(event) {
   } catch (e) {
     return new Error("Invalid body. Must be valid JSON.");
   }
+}
+
+function getUnderlyingTypeName(t: GraphQLOutputType): string {
+  if (isLeafType(t)) {
+    return t.name;
+  }
+  if (t instanceof GraphQLObjectType) {
+    return t.name;
+  }
+  if (t instanceof GraphQLUnionType) {
+    return t.name;
+  }
+  if (t instanceof GraphQLInterfaceType) {
+    return t.name;
+  }
+  if (t.ofType) {
+    return getUnderlyingTypeName(t.ofType);
+  }
+
+  throw new Error("Unexpected type " + t.toString());
+}
+
+function makeExampleMarkdown({ query, operationName, services }) {
+  const prettyQuery = prettier
+    .format(query, {
+      parser: "graphql",
+      plugins: [prettierGraphql],
+    })
+    .trim();
+  return `---
+operationName: ${operationName}
+services:${services.map((s) => `\n  - ${s.service}`).join("")}
+---
+
+\`\`\`graphql
+${prettyQuery}
+\`\`\`
+
+`;
+}
+
+async function getOctokit() {
+  const secrets = await getSecrets();
+
+  const gitHubToken = secrets.gitHub?.bearerToken;
+
+  if (!gitHubToken) {
+    // XXX: Make own error class
+    throw new Error("Internal error. Missing GitHub authentication.");
+  }
+
+  return new Octokit({ auth: gitHubToken });
 }
 
 export const handler: Handler = async (event, context) => {
@@ -79,7 +176,17 @@ export const handler: Handler = async (event, context) => {
     };
   }
 
-  const { query, parsedQuery } = bodyOrError;
+  const { query, parsedQuery, operationName } = bodyOrError;
+
+  const octokitPromise = getOctokit();
+
+  const mainBranchPromise = octokitPromise.then((octokit) =>
+    octokit.rest.repos.getBranch({
+      owner: "dwwoelfel",
+      repo: "graph-examples",
+      branch: "refs/heads/main",
+    })
+  );
 
   const serviceInfo = await fetchServiceInfo({});
 
@@ -87,14 +194,84 @@ export const handler: Handler = async (event, context) => {
 
   const schema = await getOneGraphSchema(buildNumber);
 
-  const previxes = new Set(
-    serviceInfo.data.oneGraph.services.map((s) => s.typePrefix)
-  );
+  const services = serviceInfo.data.oneGraph.services;
+
+  const errors = validate(schema, parsedQuery);
+
+  if (errors.length) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        errors: errors,
+      }),
+    };
+  }
+
+  const allTypeNames = new Set([]);
+
+  const typeInfo = new TypeInfo(schema);
+
+  const visitor = {
+    enter(_node) {
+      const type = typeInfo.getType();
+      if (type) {
+        allTypeNames.add(getUnderlyingTypeName(type));
+      }
+    },
+  };
+
+  visit(parsedQuery, visitWithTypeInfo(typeInfo, visitor));
+
+  const usedServices = new Set([]);
+
+  for (const typeName of allTypeNames) {
+    const service = services.find((s) => typeName.startsWith(s.typePrefix));
+    if (service) {
+      usedServices.add(service);
+    }
+  }
+
+  const mainBranch = await mainBranchPromise;
+
+  const newBranchRef = `refs/heads/save-query-${nanoid(8)}`;
+  const octokit = await octokitPromise;
+  await octokit.rest.git.createRef({
+    owner: "dwwoelfel", // TODO: get from environment
+    repo: "graph-examples", // TODO: get from environment
+    ref: newBranchRef,
+    sha: mainBranch.data.commit.sha,
+  });
+
+  const content = makeExampleMarkdown({
+    query,
+    operationName,
+    services: [...usedServices],
+  });
+
+  await octokit.rest.repos.createOrUpdateFileContents({
+    owner: "dwwoelfel",
+    repo: "graph-examples",
+    path: `examples/${operationName}.md`,
+    message: `Add ${operationName} example`,
+    content: Buffer.from(content).toString("base64"),
+    branch: newBranchRef,
+  });
+
+  const pull = await octokit.rest.pulls.create({
+    owner: "dwwoelfel",
+    repo: "graph-examples",
+    head: newBranchRef,
+    base: "refs/heads/main",
+    title: `Add ${operationName} example`,
+  });
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      schemaLength: Object.keys(schema.getTypeMap()).length,
+      pullRequest: {
+        url: pull.data.html_url,
+        number: pull.data.number,
+      },
     }),
   };
 };
